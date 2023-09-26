ac3d52c582b2e318494337cfa1ae8c84f29ce2a8
gouki510 <minegou0510@gmail.com>
2023-09-26 08:05:22 +0000

    add prune

--- config_pruning.py
+++ config_pruning.py
@@ -16,12 +16,13 @@ class Exp(object):
         self.is_symmetric_input = True
         self.num_epochs = 50000
         self.save_models = True 
-        self.save_every = 1000 
+        self.save_every = 2000 
 
         # Stop training when test loss is <stopping_thresh
         self.stopping_thresh = -1
         self.seed = 0 
-        self.root = Path("0926/dense2") 
+        self.root = Path("0921/prune") 
+        self.pre_root = Path("0919")
         self.model = 'mlp' # ['mlp', 'transformer']
         os.makedirs(self.root,exist_ok=True)
 
@@ -46,6 +47,29 @@ class Exp(object):
                         "only_add": lambda x,y:(x+y)}
         
         self.fn = self.fns_dict[fn_name]
+        
+        # pruning
+        self.pruner = "mag" # ["rand", "mag", "snip", "grasp", "synflow"]
+        self.sparsity = 0.4
+        self.schedule = "linear" # ["linear", "exponential"]
+        self.scope = "global" # ["global", "local"]
+        self.epochs = 1             
+        self.reinitialize =  False
+        self.train_mode = False
+        self.shuffle = False
+        self.invert = False
+        if self.is_symmetric_input:
+            self.batch_size =  (self.p**2 - self.p)//2
+        else:
+            self.batch_size =  self.p**2
+
         self.exp_name = f"{self.model}_{self.num_layers}L_{self.d_model}D_{self.p}P_\
-            {self.frac_train}F_{self.lr}LR_{self.weight_decay}WD_{self.is_symmetric_input}S_\
+            {self.frac_train}F_{self.lr}LR_{1}WD_{self.is_symmetric_input}S_\
             {self.weight_scale}WS_{fn_name}task"
+
+        self.weight_path = self.pre_root/self.exp_name/"10000.pth"
+        self.init_weight_path = self.pre_root/self.exp_name/"init.pth"
+
+        self.exp_name = f"{self.model}_{self.pruner}_PR_{self.num_layers}L_{self.d_model}D_{self.p}P_\
+            {self.frac_train}F_{self.lr}LR_{self.weight_decay}WD_{self.is_symmetric_input}S_\
+            {self.weight_scale}WS_{fn_name}task_{self.reinitialize}_reinit"
