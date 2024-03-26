4139a12690c9406a631c2328cf2dcbba9f9452ea
gouki.minegishi <gouki.minegishi@goukiminegishinoMacBook-Pro.local>
2024-03-26 21:27:09 +0900

    add slth exp

---
+++ generator.py
@@ -0,0 +1,23 @@
+def masks(module):
+    r"""Returns an iterator over modules masks, yielding the mask."""
+    for name, buf in module.named_buffers():
+        if "weight_mask" in name:
+            yield buf
+
+
+def parameters(model):
+    r"""Returns an iterator over models trainable parameters, yielding just the
+    parameter tensor.
+    """
+    for module in model.modules():
+        for param in module.parameters(recurse=False):
+            yield param
+
+
+def masked_parameters(model):
+    r"""Returns an iterator over models prunable parameters, yielding both the
+    mask and parameter tensors.
+    """
+    for module in model.modules():
+        for mask, param in zip(masks(module), module.parameters(recurse=False)):
+            yield mask, param
